bf0f5c4e6b516de8e6e9c60b009f0c37588f5fc5
Victor Balbino Machado <vbmachado123@gmail.com>
2022-01-25 19:32:51 -0300

    First Commit - Criacao do projeto

---
+++ src/main/java/br/com/tevitto/controla_guincho/Controla/Guincho/ControlaGuinchoApplication.java
@@ -0,0 +1,13 @@
+package br.com.tevitto.controla_guincho.Controla.Guincho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlaGuinchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlaGuinchoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tevitto/controla_guincho/Controla/Guincho/ControlaGuinchoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tevitto.controla_guincho.Controla.Guincho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlaGuinchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
